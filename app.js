78235734f8a01ee7417647677b4c0b82ed626e48
satou1994 <satou19940416@gmial.com>
2021-08-19 00:07:09 +0900

    仮確定

--- app.js
+++ app.js
@@ -1,59 +1,115 @@
 const express = require('express');
-const { Client } = require('pg');
 const app = express();
-
-require('dotenv').config({debug:true});
 
 app.use(express.static('public'));
 
+
 // -----------------------------------------------
-//      herokuアプリからpostgresに接続する設定
+//      Routing
 // -----------------------------------------------
+// --------  getter  -----------
+app.get('/', (req, res) => {
+  connection.query(
+    'SELECT * FROM news',
+    (error,result) => {
+      if(error) throw err;
+      res.render('index.ejs',{news:result.rows});
+    }
+  );
+});
+
+app.get('/news', (req, res) => {
+  connection.query(
+      'SELECT * FROM news order by id desc',
+      (error,result) => {
+          res.render('news.ejs',{news:comRep(result.rows)});
+      }
+  );
+});
+
+app.get('/menu', (req, res) => {
+  res.render('menu.ejs');
+});
+
+app.get('/gallery', (req, res) => {
+  connection.query(
+      'SELECT * FROM gallery ORDER BY last_update DESC',
+      (error,result) => {
+          res.render('gallery.ejs',{gallery:result.rows});
+      }
+  );
+});
+
+app.get('/staff', (req, res) => {
+  res.render('staff.ejs');
+});
+
+app.get('/hairdonation', (req, res) => {
+  res.render('hairdonation.ejs');
+});
+
+app.get('/access', (req, res) => {
+  res.render('access.ejs');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs');
+});
+
+app.get('/kids-discount-detail', (req, res) => {
+  res.render('kids-discount-detail.ejs');
+});
+  
+
+// -----------------------------------------------
+//      postgresql設定
+// -----------------------------------------------
+const { Client } = require('pg');
+require('dotenv').config({debug:true});
+
+// --------  接続情報  -----------
 const connection = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-      sslmode:'require',
-      rejectUnauthorized:false
+    sslmode:'require',
+    rejectUnauthorized:false
   }
 });
-  // const connection = new Client({
-  // host : process.env.HOST,
-  // database : process.env.DATABASE,
-  // user : process.env.ENV_USER,
-  // posrt: 5432,
-  // password : process.env.PASSWORD,
-//   ssl: {
-//       sslmode:'require',
-//       rejectUnauthorized:false
-//   }
-// });
 
-// --------  mysql接続エラーの表示設定  -----------
+// --------  接続  -----------
 connection.connect((err) => {
+  //エラー時の処理
   if(err){
       console.log('error connecting:' + err.stack);
       return;
   }
+  //接続成功時の処理
   console.log('success');
 });
 
 
 // -----------------------------------------------
-//      controller
+//      port
 // -----------------------------------------------
-// --------  get  -----------
-app.get('/', (req, res) => {
-  connection.query(
-      'SELECT * FROM news',
-      (error,result) => {
-          if(error) throw err;
-          res.render('index.ejs',{news:result.rows});
-      }
-  );
-});
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port);
+
+
+// -----------------------------------------------
+//      オリジナル
+// -----------------------------------------------
+// --------  db → html変換  -----------
+const comRep = (results) => {
+  results.forEach(result => {
+      result.comment = result.comment.replace(/\r\n/g,'<br>');
+      
+      if(result.image){
+          result.image = "<img src=\"" + result.image + "\" alt=\"ニュースの画像\">"; 
+      }
+  });
+
+  return results;
+};
