37aab3ccc51b93265c062e9c3cff0cc1cddd1983
satou1994 <satou19940416@gmial.com>
2021-08-18 11:02:32 +0900

    fifth com

--- app.js
+++ app.js
@@ -50,5 +50,8 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
-
-// app.listen(3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
